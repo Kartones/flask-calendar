533f7a8892489859f667113dd75bd5fd4c1b6f29
linuxnico <linuxnico@gmail.com>
2018-07-18 10:54:43 +0200

    create function for add user

---
+++ adduser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+#############for test only################
+import config
+from authentication import Authentication
+
+
+
+aut = Authentication(data_folder=config.USERS_DATA_FOLDER, password_salt=config.PASSWORD_SALT)
+
+
+aut.addUser('test','test')
+
+for i in aut.contents:
+	print(aut.contents[i])
+	
+
