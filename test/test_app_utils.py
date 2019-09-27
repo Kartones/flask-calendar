be6774f952fddc201d887efb713064827491d94b
David Alfonso <developer@davidalfonso.es>
2019-09-27 21:59:55 +0200

    Simplify tests by using parametrize and subtests

--- test/test_app_utils.py
+++ test/test_app_utils.py
@@ -1,3 +1,4 @@
+import pytest
 from app_utils import task_details_for_markup
 
 
@@ -5,68 +6,25 @@ SOURCE_STRING_PLACEHOLDER = "pre {} post"
 EXPECTED_STRING_PLACEHOLDER = "pre <a href=\"{}\" target=\"_blank\">{}</a> post"
 
 
-def test_http_standard_url_is_supported() -> None:
-    url = "http://test.test"
+@pytest.mark.parametrize("url, description", [
+    ("http://test.test", "standard http url"),
+    ("https://test.test", "standard https url"),
+    ("http://www.test.test", "www-prefixed url"),
+    ("http://127.0.0.1", "ip url"),
+    ("http://test.test/?test=test&test2=test2", "url with query string"),
+])
+def test_supported_task_details_for_markup(url: str, description: str) -> None:
     expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
     actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
     assert expected == actual
 
 
-def test_https_standard_url_is_supported() -> None:
-    url = "https://test.test"
+@pytest.mark.parametrize("url, description", [
+    ("http://test.test/#some=thing", "hash url"),
+    ("http://localhost/", "localhost url"),
+    ("http://test.test:8000", "specified port url"),
+])
+def test_unsupported_task_details_for_markup(url: str, description: str) -> None:
     expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
     actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
-    assert expected == actual
-
-
-def test_www_prefixed_url_is_supported() -> None:
-    url = "http://www.test.test"
-    expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
-    actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
-    assert expected == actual
-
-
-def test_ip_url_is_supported() -> None:
-    url = "http://127.0.0.1"
-    expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
-    actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
-    assert expected == actual
-
-
-def test_querystring_parameters_url_is_supported() -> None:
-    url = "http://test.test/?test=test&test2=test2"
-    expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
-    actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
-    assert expected == actual
-
-
-# Unsupported urls
-
-
-def test_hash_url_is_unsupported() -> None:
-    url = "http://test.test/#some=thing"
-    expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
-    actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
-    assert expected != actual
-
-
-def test_localhost_url_is_unsupported() -> None:
-    url = "http://localhost/"
-    expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
-    actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
-    assert expected != actual
-
-
-def test_specified_port_url_is_unsupported() -> None:
-    url = "http://test.test:8000"
-    expected = EXPECTED_STRING_PLACEHOLDER.format(url, url)
-    actual = task_details_for_markup(SOURCE_STRING_PLACEHOLDER.format(url))
-
     assert expected != actual
