b494079046744e6b656643387c8cc4f356a1c18f
Kartones <diego@kartones.net>
2019-09-28 19:30:57 +0200

    Skip test files from mypy linter

--- test/test_linters.py
+++ test/test_linters.py
@@ -2,7 +2,8 @@ import subprocess
 import sys
 
 
-SOURCE_PATHS = "*.py test"
+CODE_SOURCE_PATH = "*.py"
+TEST_SOURCE_PATH = "test"
 
 
 def test_flake8_compliance() -> None:
@@ -10,7 +11,7 @@ def test_flake8_compliance() -> None:
                                             shell=True,
                                             stderr=sys.stderr).decode("ascii").replace("\n", "")
 
-    result = subprocess.call("{} {}".format(flake8_binary, SOURCE_PATHS),
+    result = subprocess.call("{} {}".format(flake8_binary, " ".join([CODE_SOURCE_PATH, TEST_SOURCE_PATH])),
                              shell=True,
                              stdout=sys.stdout,
                              stderr=sys.stderr)
@@ -24,7 +25,7 @@ def test_mypy_compliance() -> None:
 
     result = subprocess.call("{} --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs"
                              " --warn-redundant-casts --warn-return-any --warn-unused-ignores --strict-optional"
-                             " {}".format(mypy_binary, SOURCE_PATHS),
+                             " {}".format(mypy_binary, CODE_SOURCE_PATH),
                              shell=True,
                              stdout=sys.stdout,
                              stderr=sys.stderr)
