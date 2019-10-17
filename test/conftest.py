8adee071a973e7163b4098168cab9cd00899bd3f
David Alfonso <developer@davidalfonso.es>
2019-10-18 00:38:28 +0200

    Add tests for login view and client/app fixtures
    - Add two pytest fixtures: app and client.
    - The app fixture makes use of the new create_app() factory method.
    - Check correct redirections after login.
    - Check correct cookie is set after valid login.
    - Check root redirects to default calendar when logged in.
    - Don't delay failed logins in tests.

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from flask import Flask
+from flask.testing import FlaskClient
 from flask_calendar.app import create_app
 
 
 @pytest.fixture
 def app() -> Flask:
-    return create_app()
+    return create_app({
+        'TESTING': True,
+        'FAILED_LOGIN_DELAY_BASE': 0,
+    })
+
+
+@pytest.fixture
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
